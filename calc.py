1bb2a382e309848b8262c294de3069c87923f95e
w4kr10 <mtalin001@gmail.com>
2025-07-31 02:20:25 -0400

    Add files via upload

---
+++ calc.py
@@ -0,0 +1,28 @@
+while True:
+    num1 = float(input("Enter the first number: "))
+    num2 = float(input("Enter the second number: "))
+    operation = input("Enter an operation (+, -, *, /): ")
+
+    # Check if the user wants to exit
+    if operation.lower() == 'e':
+        print("Exiting the calculator. Goodbye!")
+        break
+
+# Perform calculation based on operation
+    if operation == '+':
+        result = num1 + num2
+        print(f"{num1} + {num2} = {result}")
+    elif operation == '-':
+        result = num1 - num2
+        print(f"{num1} - {num2} = {result}")
+    elif operation == '*':
+        result = num1 * num2
+        print(f"{num1} * {num2} = {result}")
+    elif operation == '/':
+        if num2 != 0:
+            result = num1 / num2
+            print(f"{num1} / {num2} = {result}")
+        else:
+            print("Error: Division by zero is not allowed.")
+    else:
+        print("Invalid operator. Please use one of +, -, *, or /.")
